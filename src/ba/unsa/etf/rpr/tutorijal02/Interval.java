635e20b2e7e9abff4ccef263c4578af723f54e9e
Samir <sdosljak1@etf.unsa.ba>
2019-10-23 16:36:38 +0200

    Dodani konstruktori

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -2,5 +2,22 @@ package ba.unsa.etf.rpr.tutorijal02;
 
 public class Interval {
 
+    private double pocetna;
+    private double krajnja;
+    private boolean pocetnaPripada;
+    private boolean krajnjaPripada;
+
+    public Interval(double pocetna, double krajnja, boolean pocetnaPripada, boolean krajnjaPripada) {
+        if (pocetna > krajnja) throw new IllegalArgumentException;
+        this.pocetna = pocetna;
+        this.krajnja = krajnja;
+        this.pocetnaPripada = pocetnaPripada;
+        this.krajnjaPripada = krajnjaPripada;
+    }
+
+    public Interval() {
+        pocetna = 0;
+        krajnja = 0;
+    }
 
 }
